fca54ebabbb0480394d6e7f1cbabc2956515e7e1
João Langhanz <joaopaulo.lang@gmail.com>
2018-11-26 01:19:32 -0200

    Algoritmo Genético implementado.

--- LabirintoDasMoedasT2/src/model/Agente.java
+++ LabirintoDasMoedasT2/src/model/Agente.java
@@ -19,6 +19,7 @@ public class Agente extends Objeto {
 	private int pontuacao;
 	private Tabuleiro tabuleiro;
 	private EstadoDoAgente estadoAtual;
+	private Perceptron rede = new Perceptron();
 
 	public Agente(int posicaoX, int posicaoY, Tabuleiro tabuleiro) {
 		super(posicaoX, posicaoY, TipoDeObjeto.AGENTE);
@@ -26,6 +27,7 @@ public class Agente extends Objeto {
 		this.sacosDeMoedas = new ArrayList<SacoDeMoedas>();
 		this.pontuacao = 0;
 		this.estadoAtual = EstadoDoAgente.PROCURANDO_PORTA;
+		genetico();
 	}
 
 	public List<SacoDeMoedas> getSacosDeMoedas() {
@@ -84,7 +86,7 @@ public class Agente extends Objeto {
 						getTabuleiro().moverAgenteComPulo(direcao);
 					}
 				} else {
-					System.out.println("Movimento não permitido!");
+					System.out.println("Movimento nï¿½o permitido!");
 				}
 	
 			} else if (objeto.getTipo() == TipoDeObjeto.SACO_DE_MOEDAS) {
@@ -102,13 +104,13 @@ public class Agente extends Objeto {
 			tabuleiro.imprimeTabuleiroVisivelPeloAgente();
 	
 			try {
-				Thread.sleep(1000); //?? criar variável ou deixar aqui mesmo?
+				Thread.sleep(1000); //?? criar variï¿½vel ou deixar aqui mesmo?
 			} catch (InterruptedException e) {
 				e.printStackTrace();
 			}
 			
 		} else {
-			System.out.println("Movimento não permitido!");
+			System.out.println("Movimento nï¿½o permitido!");
 		}
 	}
 	
@@ -255,12 +257,14 @@ public class Agente extends Objeto {
 	}
 	
 	public Direcao rodarRedeNeural(Direcao ultimaDirecao, int iteracao) {
-		Perceptron rede = new Perceptron();
+		
 
 		// Livre = 4 Muro = 3 Buraco = 1 Saco = 2 Porta = 5
 		int valorSul   = retornaValorObjetoParaRede(getTabuleiro().getObjetoPelaDirecao(getCoordenadas(), Direcao.SUL));
 		int valorLeste = retornaValorObjetoParaRede(getTabuleiro().getObjetoPelaDirecao(getCoordenadas(), Direcao.LESTE));
 		int valorOeste = retornaValorObjetoParaRede(getTabuleiro().getObjetoPelaDirecao(getCoordenadas(), Direcao.OESTE));
+
+		setPesosNeuronios();
 		
 		// Calcula valores camada de entrada
 		int valorEntradaNeuronio0 = rede.getNeuronios().get(0).calculaY(valorSul, valorLeste, valorOeste);
@@ -332,34 +336,63 @@ public class Agente extends Objeto {
 			getTabuleiro().imprime();
 		} else {
 			setEstadoAtual(EstadoDoAgente.GAME_OVER);
+			atual[33] = pontuacao;
 		}
 		
 		return direcao;
 	}
 
 	private void imprimeMovimento(Direcao direcao, boolean moveComPulo, int iteracao) {
-		System.out.println("Iteração " + iteracao + " - Movimento efetuado pelo agente: Direção " + (direcao == null ? "inválida" : direcao.toString()) + (moveComPulo ? " com pulo" : " sem pulo") + "\n\n");
+		System.out.println("Iteraï¿½ï¿½o " + iteracao + " - Movimento efetuado pelo agente: Direï¿½ï¿½o " + (direcao == null ? "invï¿½lida" : direcao.toString()) + (moveComPulo ? " com pulo" : " sem pulo") + "\n\n");
 	}
+
+	/**
+         * Seta os pesos dos nouronios de acordo com os valores gerados no AG
+         */
+        private void setPesosNeuronios(){
+            
+            for(int i=0;i<32;i++){
+                rede.getNeuronios().get(i).setW0(atual[i]);
+                rede.getNeuronios().get(i).setW1(atual[i+1]);
+                rede.getNeuronios().get(i).setW2(atual[i+2]);
+                rede.getNeuronios().get(i).setW3(atual[i+3]);
+                i += 3;
+            }
+            
+        }
 
 	// genetico ---------------------------------------------------------------
-	private int[] populacao = new int[32];
-	private int[] atual = new int[32];
-	private int[] melhor = new int[32];
+	private double[] populacao = new double[33];
+	private double[] atual = new double[33];
+	private double[] melhor1 = new double[33];
+	private double[] melhor2 = new double[33];
 
-	public boolean genetico() {
-		// System.out.println("############# GENETICO #######################");
-
-		melhor[31] = 1000;
+	public void genetico() {
+		
 		popular();
-		int c = 0;
 
-		while (!aptdar() || c < 200000) {
-			mutar();
-			c++;
-		}
-
-		return false;
+		
 	}
+        
+        /**
+         * Gera a proxima geraÃ§ao de pesos
+         */
+        public void proximaGeracao(){
+            
+            if(melhor1[33] > melhor2[33]){
+                if(atual[33] > melhor1[33]){
+                    melhor1 = atual.clone();
+                }
+            }else{
+                if(atual[33] > melhor2[33]){
+                    melhor2 = atual.clone();
+                }
+            }
+            
+            cruzar();
+            mutar();
+            
+        }
 
 	/**
 	 * 
@@ -370,31 +403,16 @@ public class Agente extends Objeto {
 	 */
 	public void popular() {
 
-		int cont0 = 0, cont1 = 0, cont2 = 0, cont3 = 0;
 		Random rand = new Random();
-		int r;
-
-		for (int i = 0; i < 31; i++) {
-			do {
-				r = rand.nextInt(4);
-			} while ((r == 0 && cont0 >= 4) || (r == 1 && cont1 >= 4) || (r == 2 && cont2 >= 4)
-					|| (r == 3 && cont3 >= 4));
-
-			populacao[i] = r;
-
-			if (r == 0 && cont0 < 4)
-				cont0++;
-			if (r == 1 && cont1 < 4)
-				cont1++;
-			if (r == 2 && cont2 < 4)
-				cont2++;
-			if (r == 3 && cont3 < 4)
-				cont3++;
-		}
-
+                
+                for (int i=0;i<31;i++) {
+                    populacao[i] = rand.nextDouble();
+                }
+                
 		atual = populacao.clone();
-		melhor = populacao.clone();
-		melhor[31] = 1000;
+		melhor1 = populacao.clone();
+		melhor2 = populacao.clone();
+		//melhor[31] = 1000;
 	}
 
 	/**
@@ -407,23 +425,35 @@ public class Agente extends Objeto {
 	 */
 	public void mutar() {
 		Random rand = new Random();
-		int t1 = rand.nextInt(31);
-		int t2 = rand.nextInt(31);
+		int t1 = rand.nextInt(32);
+		int t2 = rand.nextInt(32);
 		
-		atual = melhor.clone();
+//		atual = melhor.clone();
 		
-		// Troca duas posiçoes aleatorias
-		int a = atual[t1];
-		int b = atual[t2];
+		// Troca duas posicoes aleatorias
+//		double a = atual[t1];
+//		double b = atual[t2];
 		
-		atual[t1] = b;
-		atual[t2] = a;
+		atual[t1] = rand.nextDouble();
+		atual[t2] = rand.nextDouble();
 	}
+        
+        public void cruzar(){
+            
+            for(int i=0;i<32;i++){
+                if(i<8 || (i>=16 && i<24)){
+                    atual[i] = melhor1[i];
+                }else{
+                    atual[i] = melhor2[i];
+                }
+            }
+            
+        }
 
 	/**
 	 * 
 	 * 
-	 * Faz o somatorio da diferença de todos os baus com todos os baus
+	 * Faz o somatorio da diferenca de todos os baus com todos os baus
 	 * 
 	 * 
 	 * @return
@@ -446,27 +476,27 @@ public class Agente extends Objeto {
 		}
 	 */
 		// int diferenca = Math.abs((Math.abs(soma0+soma1))-(Math.abs(soma2+soma3)));
-		int difA = 0;
-		int d;
-		
-		// Soma a diferenca de todos com todos
-		for (int t = 0; t < 4; t++) {
-			d = somas[t];
-			for (int g = 0; g < 4; g++) {
-				difA += Math.abs(d - somas[g]);
-			}
-		}
-
-		int diferenca = difA;
-		atual[16] = diferenca;
-		if (diferenca == 0) { // Achou a resposta
-			melhor = atual.clone(); // Elitiza
-			return true;
-		}
-		
-		if (diferenca < melhor[16]) { // Achou um melhor
-			melhor = atual.clone(); // Elitiza
-		}
+//		int difA = 0;
+//		int d;
+//		
+//		// Soma a diferenca de todos com todos
+//		for (int t = 0; t < 4; t++) {
+//			d = somas[t];
+//			for (int g = 0; g < 4; g++) {
+//				difA += Math.abs(d - somas[g]);
+//			}
+//		}
+//
+//		int diferenca = difA;
+//		atual[16] = diferenca;
+//		if (diferenca == 0) { // Achou a resposta
+//			melhor = atual.clone(); // Elitiza
+//			return true;
+//		}
+//		
+//		if (diferenca < melhor[16]) { // Achou um melhor
+//			melhor = atual.clone(); // Elitiza
+//		}
 
 		return false;
 	}
@@ -487,7 +517,7 @@ public class Agente extends Objeto {
 			
 			if (estadoAtual == EstadoDoAgente.FORA_DO_LABIRINTO) {
 				System.out.println("-*--*--*--*--*--*--*--*--*--*--*-");
-				System.out.println("Labirinto concluído com sucesso!");
+				System.out.println("Labirinto concluï¿½do com sucesso!");
 				System.out.println("-*--*--*--*--*--*--*--*--*--*--*-");
 
 			} else if (estadoAtual == EstadoDoAgente.GAME_OVER) {
@@ -554,7 +584,7 @@ public class Agente extends Objeto {
 
 		if (estadoAtual == EstadoDoAgente.FORA_DO_LABIRINTO) {
 			System.out.println("-*--*--*--*--*--*--*--*--*--*--*-");
-			System.out.println("Labirinto concluído com sucesso!");
+			System.out.println("Labirinto concluï¿½do com sucesso!");
 			System.out.println("-*--*--*--*--*--*--*--*--*--*--*-\n\n");
 
 		} else if (estadoAtual == EstadoDoAgente.GAME_OVER) {
